18516bec4fb3fe56e2648ae4cc8f14bdbd0f1a9c
shivraj2328 <shivrajjagtap6@gmail.com>
2026-07-21 22:21:48 +0530

    added Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,60 @@
+import Hero from "../components/Hero/Hero";
+import FeatureCard from "../components/FeatureCard/FeatureCard";
+
 function Home() {
-  return <h1>Home Page</h1>;
+
+    return (
+
+        <>
+
+            <Hero />
+
+            <section className="container my-5">
+
+                <h2 className="text-center mb-5">
+                    Our Features
+                </h2>
+
+                <div className="row">
+
+                    <FeatureCard
+                        title="Crop Recommendation"
+                        description="AI recommends the best crop."
+                    />
+
+                    <FeatureCard
+                        title="Weather Information"
+                        description="Live weather updates."
+                    />
+
+                    <FeatureCard
+                        title="Soil Health"
+                        description="Analyze soil quality."
+                    />
+
+                    <FeatureCard
+                        title="Irrigation"
+                        description="Smart irrigation advice."
+                    />
+
+                    <FeatureCard
+                        title="Dashboard"
+                        description="View all analytics."
+                    />
+
+                    <FeatureCard
+                        title="Prediction History"
+                        description="Access previous predictions."
+                    />
+
+                </div>
+
+            </section>
+
+        </>
+
+    );
+
 }
 
 export default Home;
