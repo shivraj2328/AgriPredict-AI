51968e956e72f33beaee078d12c6d68976f8b233
shivraj2328 <shivrajjagtap6@gmail.com>
2026-07-21 22:22:43 +0530

    Added MainLayout

---
+++ client/src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+function MainLayout() {
+    return (
+        <>
+            <Navbar />
+
+            <main>
+                <Outlet />
+            </main>
+
+            <Footer />
+        </>
+    );
+}
+
+export default MainLayout;
