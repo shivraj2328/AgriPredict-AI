3be83435a8168c8a46af5cccfa4b15d13535b8f8
shivraj2328 <shivrajjagtap6@gmail.com>
2026-07-28 20:59:12 +0530

    Created WeatherWidget as a reusable component for dashboard

---
+++ client/src/components/Dashboard/WeatherWidget/WeatherWidget.jsx
@@ -0,0 +1,27 @@
+function WeatherWidget(){
+
+return(
+
+<div className="card shadow-sm">
+
+<div className="card-body">
+
+<h4>Pune</h4>
+
+<h1>28°C</h1>
+
+<p>Humidity : 72%</p>
+
+<p>Rainfall : 180 mm</p>
+
+<p>☀ Sunny</p>
+
+</div>
+
+</div>
+
+);
+
+}
+
+export default WeatherWidget;
