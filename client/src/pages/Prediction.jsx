a6b2302fc08a0496d5449d00bf6051d296790117
shivraj2328 <shivrajjagtap6@gmail.com>
2026-08-04 22:01:44 +0530

    Created Prediction page using reusable component like form and result

--- client/src/pages/Prediction.jsx
+++ client/src/pages/Prediction.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
+import DashboardLayout from "../layouts/DashboardLayout";
+import PredictionForm from "../components/Dashboard/PredictionForm/PredictionForm";
+import PredictionResult from "../components/Dashboard/PredictionResult/PredictionResult";
+
 function Prediction() {
-  return <h1>Prediction Page</h1>;
+
+    const [formData, setFormData] = useState({
+        nitrogen: "",
+        phosphorus: "",
+        potassium: "",
+        temperature: "",
+        humidity: "",
+        rainfall: "",
+        ph: ""
+    });
+
+    return (
+
+        <DashboardLayout>
+
+            <div className="container-fluid p-4">
+
+                <h2 className="mb-4">
+                    🌱 Crop Prediction
+                </h2>
+
+                <PredictionForm
+                    formData={formData}
+                    setFormData={setFormData}
+                />
+
+                <PredictionResult />
+
+            </div>
+
+        </DashboardLayout>
+
+    );
+
 }
 
 export default Prediction;
