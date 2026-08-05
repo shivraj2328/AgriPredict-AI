fe48be33c673bdd3e1c96d5adb2d9376b807fa63
shivraj2328 <shivrajjagtap6@gmail.com>
2026-08-05 22:19:40 +0530

    Modified routing from dashboard to quick prediction

--- client/src/components/Dashboard/QuickPrediction/QuickPrediction.jsx
+++ client/src/components/Dashboard/QuickPrediction/QuickPrediction.jsx
@@ -1,19 +1,34 @@
+import { useNavigate } from "react-router-dom";
+
 function QuickPrediction() {
-  return (
-    <div className="card shadow-sm">
-      <div className="card-body">
-        <h4>Quick Prediction</h4>
 
-        <p>
-          Start a new crop prediction using soil and weather data.
-        </p>
+    const navigate = useNavigate();
 
-        <button className="btn btn-success">
-          Start Prediction
-        </button>
-      </div>
-    </div>
-  );
+    return (
+
+        <div className="card shadow-sm">
+
+            <div className="card-body">
+
+                <h4>Quick Prediction</h4>
+
+                <p>
+                    Start a new crop prediction using soil and weather data.
+                </p>
+
+                <button
+                    className="btn btn-success"
+                    onClick={() => navigate("/prediction")}
+                >
+                    Start Prediction
+                </button>
+
+            </div>
+
+        </div>
+
+    );
+
 }
 
 export default QuickPrediction;
