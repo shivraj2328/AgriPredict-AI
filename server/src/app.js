cd17924b9246ff396adcd04dd6ab3b48a0680ccd
shivraj2328 <shivrajjagtap6@gmail.com>
2026-08-16 23:31:12 +0530

    Initialized express server

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const connectDB = require("./config/db");
+const healthRoutes = require("./routes/health.routes");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.use("/api/health", healthRoutes);
+
+app.get("/", (req, res) => {
+    res.send("AgriPredict API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`AgriPredict server running on port ${PORT}`);
+});
